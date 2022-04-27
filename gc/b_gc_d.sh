15484a62200b68bbd8936036b369907a25df5f71
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-04-27 19:52:50 +0800

    damm sh, https://stackoverflow.com/a/24464715

--- gc/b_gc_d.sh
+++ gc/b_gc_d.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version=$(cat VERSION)
 docker build -t "siakbary/dockergc:debian-$version" -f debian .;
-if [ "$1" == "push" ];then
+if [ "$1" = "push" ];then
  docker push siakbary/dockergc:debian-$version
 fi
