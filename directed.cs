7e98c26f20c2b576ebaf78d0f5e09bfd09d37496
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-05-07 18:02:19 +0800

    add sg server

--- directed.cs
+++ directed.cs
@@ -2,12 +2,16 @@ import System;
 import System.Windows.Forms;
 import Fiddler;
 import System.Text.RegularExpressions;
+
 class Handlers
 {
     static function OnBeforeRequest(oS: Session) {
         if(oS.host.EndsWith(".yuanshen.com") || oS.host.EndsWith(".hoyoverse.com") || oS.host.EndsWith(".mihoyo.com")) {
-            // This "game.yuuki.me" can also be replaced with another ip/domain server.
-            oS.host = "game.yuuki.me"; 
+            //This can also be replaced with another ip/domain server.
+            //oS.host = "game.yuuki.me";
+            oS.host = "sg2.game.yuuki.me";
+            //oS.host = "2.0.0.100";
+            //oS.host = "localhost";
         }
     }
 };
